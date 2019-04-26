180e1792bc08297eadc8dcc906143be16d753b2f
naveen <naveen@DESKTOP-QEPGQB5>
2019-04-26 19:44:07 +0530

    simpledemo

---
+++ Test/src/test/java/com/abc/ng/Testdemo.java
@@ -0,0 +1,13 @@
+package com.abc.ng;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Testdemo 
+{
+	@SuppressWarnings("unused")
+	@Test
+	public void main() {
+		ChromeDriver d=new ChromeDriver();
+	}
+}
